5c9d48337589674764e9df663515384ea3c87c6a
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-05-05 08:10:35 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const email = document.querySelector("#input-email");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    if (email.value === " " || email.value.indexOf("@") == -1 || email.value == null) {
+    if (email.value === " " || email.value.indexOf("@") === -1 || email.value == null) {
       email.nextElementSibling.classList.add("input-email__warning--visible");
 	  email.focus()
 	  form.lastElementChild.classList.add("btn-submit--error-icon")
