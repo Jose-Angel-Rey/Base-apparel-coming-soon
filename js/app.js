d4608588da396fe59cbcd2abdb313b77dff6416e
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-05-05 08:09:09 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
-const form = querySelector(".email-form")
-const email = querySelector("#input-email")
+const form = document.querySelector(".email-form");
+const email = document.querySelector("#input-email");
 
-form,addEventListener("submit", (e) =>{
-	e.preventDefault()
-	
-	if (email.value === " "  || email.value.indexOf("@") == -1 || email.value == null){
-	console.log("Funcionando")
-	email.nextElementSibling.classList.add("input-email__warning--visible")
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (email.value === " " || email.value.indexOf("@") == -1 || email.value == null) {
+      email.nextElementSibling.classList.add("input-email__warning--visible");
+	  email.focus()
+	  form.lastElementChild.classList.add("btn-submit--error-icon")
+    }
+  });
+
+  email.addEventListener("blur", (e) => {
+	if(e.target.nextElementSibling.classList.contains("input-email__warning--visible")){
+	e.target.nextElementSibling.classList.remove("input-email__warning--visible");
+	form.lastElementChild.classList.remove("btn-submit--error-icon")
 	}
-})
+  })
