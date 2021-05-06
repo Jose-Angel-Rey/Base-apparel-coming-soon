31668a3c51e3d03df7166455453881277c62e1c9
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-05-06 09:28:04 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,18 +1,15 @@
-const form = document.querySelector(".email-form");
-const email = document.querySelector("#input-email");
+const form = document.getElementById("form");
+const email = document.getElementById("input-email");
+let emailPattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 
 form.addEventListener("submit", (e) => {
+  if (!email.value.match(emailPattern) || email.value === "") {
     e.preventDefault();
-    if (email.value === " " || email.value.indexOf("@") === -1 || email.value == null) {
-      email.nextElementSibling.classList.add("input-email__warning--visible");
-	  email.focus()
-	  form.lastElementChild.classList.add("btn-submit--error-icon")
-    }
-  });
-
-  email.addEventListener("blur", (e) => {
-	if(e.target.nextElementSibling.classList.contains("input-email__warning--visible")){
-	e.target.nextElementSibling.classList.remove("input-email__warning--visible");
-	form.lastElementChild.classList.remove("btn-submit--error-icon")
-	}
-  })
+    email.nextElementSibling.classList.add("input-email__warning--visible");
+    form.lastElementChild.classList.add("btn-submit--error-icon");
+    email.focus();
+  } else {
+    email.nextElementSibling.classList.remove("input-email__warning--visible");
+    form.lastElementChild.classList.remove("btn-submit--error-icon");
+  }
+});
